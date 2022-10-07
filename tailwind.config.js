55eac3f110aae3a56a8e0db161f1d23691ca356b
Bruce Giovando <bgiovand@gmail.com>
2022-10-06 19:40:28 -0700

    updated jsx in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@
 const colors = require("tailwindcss/colors")
 
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./pages/**/*.{html,js}"],
+  content: ["./src/**/*.{html,jsx}", "./pages/**/*.{html,jsx}"],
   theme: {},
   extend: {
     colors: {
