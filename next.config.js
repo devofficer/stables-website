97e74853d67da5c411da448b6da99c89daaf4e3d
Bruce Giovando <bgiovand@gmail.com>
2022-10-14 09:33:03 -0700

    removed document folder added redirect to aws

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+   async redirects() {
+    return [
+      {
+        source: "/SalesSheet",
+        permanent: true,
+        destination:
+          "https://getstables.s3-us-west-2.amazonaws.com/SalesSheet.pdf",
+      },
+    ];
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
