e0f764e41fb5a67e589b4320a58d8227e0b6535d
nikolaytoplev <nikolaytoplev@gmail.com>
2022-11-24 07:15:25 +0900

    fix: sanity client default params

--- client.js
+++ client.js
@@ -2,8 +2,8 @@
 import sanityClient from "@sanity/client"
 
 export default sanityClient({
-  projectId: process.env.SANITY_API_PROJECT_ID,
-  dataset: process.env.SANITY_API_DATASET,
+  projectId: process.env.SANITY_API_PROJECT_ID || "hwmnpy3d",
+  dataset: process.env.SANITY_API_DATASET || "production",
   token: process.env.SANITY_API_WRITE_TOKEN,
   useCdn: true, // `false` if you want to ensure fresh data
   apiVersion: "2021-03-25", // use a UTC date string
